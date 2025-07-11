434755823047b8a1f0247ebaec870c3389fd4483
Bob Jack <bobjack1313@gmail.com>
2025-07-10 22:09:26 -0500

    Displays bits, can cut opcode separating for types

--- tiny_mips_cpu.cpp
+++ tiny_mips_cpu.cpp
@@ -11,6 +11,7 @@
 
 #include "tiny_mips_cpu.h"
 #include <iostream>
+#include <bitset>
 
 using namespace std;
 
@@ -18,7 +19,8 @@ using namespace std;
 TinyMipsCPU::TinyMipsCPU() {
   cerr << "TinyMipsCPU is active\n";
 }
-
+// Display func declaration
+void displayBits(uint32_t value, int bits);
 
 // Need a function to load the instructions into the cpu class
 void TinyMipsCPU::loadProgram(const vector<uint32_t>& instructions) {
@@ -26,40 +28,81 @@ void TinyMipsCPU::loadProgram(const vector<uint32_t>& instructions) {
     pc = 0;
 }
 
+// Will cycle through each instruction step until completion
 void TinyMipsCPU::executeProgram() {
-    
+    // Heartbeat loop for each step
+    while(performStep());
 }
 
-// Need a function for execution loop (heartbeat)
+// Works through the instruction | picks type | segments
+bool TinyMipsCPU::performStep() {
+    // Reject badly formed instructions - not div by 4
+    if (pc >= instructionMemory.size() * 4) return false;
 
+    // Get the current instruction from pc
+    uint32_t current_instruction = instructionMemory[pc / 4];
+    // Extract opcode from first 6 bit
+    uint32_t opcode = getOpcode(current_instruction);
 
-// Need a funtion to run through each instruction segment
+    // Debugging
+    displayBits(opcode, 6);
 
+    // Separate 0 for RType | 2, 3 for J Type | Remaining are IType
+    if (opcode == 0) {
+        runStyleRType(current_instruction); 
 
-// Separate function to run Rtype
+    } else if (opcode == 2 || opcode == 3) {
+        runStyleJType(current_instruction); 
 
+    } else {
+        runStyleIType(current_instruction);
+    }
+    // Increment pc + 4
+    pc += 4;
+    return true;
+}
 
-// Separate function to run Itype
+// Dummy implementations for now:
+void TinyMipsCPU::runStyleRType(uint32_t instr) {
+    cout << "R-type stub\n";
+}
 
+void TinyMipsCPU::runStyleIType(uint32_t instr) {
+    cout << "I-type stub\n";
+} 
 
-// Separate function to run Jtype
-
+void TinyMipsCPU::runStyleJType(uint32_t instr) { 
+    cout << "J-type stub\n";
+} 
 
 // Fucntion to display the register
 
 
 
-// Function to display the value in the register
 
+// Helper function to extract the bits from instruction
+uint32_t extractBits(uint32_t value, int start, int length) {
+    uint32_t bitShifted = value >> start;
+    uint32_t mask  = ((1 << length) - 1);
+    // Debug
+    displayBits(bitShifted, 6); 
+    return bitShifted & mask;
+}
 
-// Fetcher functions for each of the instruction segments
+uint32_t TinyMipsCPU::getOpcode(uint32_t instr) const {
+    return extractBits(instr, 26, 6);
+}
 
+// Debugging visual bit display
+void displayBits(uint32_t bits, int numBits) {
+    // Mask to keep only the numBits lower bits
+    uint32_t mask = (numBits >= 32) ? 0xFFFFFFFF : ((1u << numBits) - 1);
+    bits &= mask;
 
-//
-//
-//
-//
-//
-//
-//
+    // Always use 32 and slice from right
+    bitset<32> b(bits); 
+    string output = b.to_string().substr(32 - numBits);
 
+    cout << "Decimal: " << bits << "\n";
+    cout << "Binary (" << numBits << " bits): " << output << "\n";
+}
--- tiny_mips_cpu.h
+++ tiny_mips_cpu.h
@@ -1,5 +1,31 @@
+/*------------------------------------------------------------------------------
+  File:        tiny_mips_cpu.h
+  Project:     Tiny MIPS CPU - CSCE 5610 Group Project
+  Purpose:     Header file for the cpu driver of cpu simulator. Declares the 
+               interface for a simple single-cycle MIPS CPU simulator.
 
+  Authors:     Souban Ahmed, Bhargav Alimili, Bob Jack, Janaki Ramaiah Venigalla
+  Group:       CSCE5610 - Group 2
+  Instructor:  Dr. Beilei Jiang
+  Date:        July 2025
 
+  Description:
+               Defines the TinyMipsCPU class that simulates execution of 32-bit 
+               binary MIPS instructions. Supports a basic register file, 
+               instruction memory, and memory for load/store operations. 
+               Implements 10 instructions:
+
+               - R-type: add, sub, and, or, slt, nor
+               - I-type: lw, sw, beq
+               - J-type: j
+
+               The class is designed to test output from the Tiny MIPS 
+               Assembler and simulate how those instructions would 
+               execute in a basic MIPS-compatible processor model.
+
+  Dependencies:
+    - <string>, <cstdint>, <vector>, <string>, <array> 
+  -----------------------------------------------------------------------------*/
 #ifndef TINY_MIPS_CPU_H
 #define TINY_MIPS_CPU_H
 
@@ -18,6 +44,9 @@ public:
     // Run the program until completion - jumps to invalid PC or runs out of code
     void executeProgram(); 
 
+    // Execute one instruction and update PC
+    bool performStep(); 
+
 private:
     // Register range from 0-31
     std::array<uint32_t, 32> registers; 
@@ -26,8 +55,18 @@ private:
     // Simplified flat memory
     std::vector<uint8_t> memory;
     // Program counter           
-    uint32_t pc;     
+    uint32_t pc;  
+    
+    // Instruction decoding helpers accesses
+    uint32_t getOpcode(uint32_t instruction) const; 
+
+    // Instruction implementations
+    void runStyleRType(uint32_t instruction); 
+    void runStyleIType(uint32_t instruction);
+    void runStyleJType(uint32_t instruction); 
+
+
 
 };
    
-#endif // End TINY_MIPS_CPU_H 
+#endif // TINY_MIPS_CPU_H 
